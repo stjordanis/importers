dbb2786f34f9c1e4a9cb7ccb55912dcf83fa315e
Fiona Spooner <spoonerfiona@gmail.com>
2021-05-14 09:40:55 +0100

    Uploading files and __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import os
+# Dataset constants.
+DATASET_NAME = "United Nations Sustainable Development Goals Indicators"
+DATASET_AUTHORS = "United Nations"
+DATASET_VERSION = "2021.03"
+DATASET_LINK = "https://unstats.un.org/sdgs/indicators/database/" # Have to request dataset by email
+DATASET_RETRIEVED_DATE = "10-May-2021"
+DATASET_DIR = os.path.dirname(__file__) #Multiple .xlsx files - one per SDG
+DATASET_NAMESPACE = f"{DATASET_DIR.split('/')[-1]}@{DATASET_VERSION}"
+CONFIGPATH = os.path.join(DATASET_DIR, 'config')
+INPATH = os.path.join(DATASET_DIR, 'input')
+OUTPATH = os.path.join(DATASET_DIR, 'output')
